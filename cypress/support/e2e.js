9d012fafeadde4d9698d5e3e7dbd246a25f22bb7
Brandon Ogle <oglebrandon@gmail.com>
2025-12-04 15:59:31 -0800

    Align Cypress clock with backend test date

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,14 @@
 // cypress/support/e2e.js
 import "./commands";
+
+// Freeze frontend time and align backend clock via X-Test-Date.
+beforeEach(() => {
+  const now = new Date(2025, 10, 15);
+  const isoDate = now.toISOString().split("T")[0];
+
+  cy.intercept("**", (req) => {
+    req.headers["x-test-date"] = isoDate;
+  });
+
+  cy.clock(now.getTime(), ["Date"]);
+});
